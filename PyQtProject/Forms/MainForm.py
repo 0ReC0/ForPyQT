2e8e4ee7f8d8f2270f623e99af986abeb56f6a0b
ReC <kitanin.ser@mail.ru>
2021-03-06 12:28:07 +0300

    Added showing tables names from db and info from tables

--- PyQtProject/Forms/MainForm.py
+++ PyQtProject/Forms/MainForm.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QMainWindow, QFileDialog, QMessageBox, QTabWidget, QGridLayout, QTableWidget
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QMessageBox, QTabWidget, QGridLayout, QTableWidget, \
+    QTableWidgetItem, QWidget
 from PyQtProject.UIs.MainUI_ui import Ui_MainWindow
 import sqlite3
 
@@ -30,17 +31,47 @@ class MainForm(QMainWindow, Ui_MainWindow):
                 raise Exception("Нет таблиц в базе данных")
 
             self.notLoadDbLabel.deleteLater()
-            self.tables_tab_widget = QTabWidget()
+            self.tables_tab_widget = QTabWidget(self)
+            self.gridLayout.addWidget(self.tables_tab_widget)
 
             for name in tables_names:
-                table_widget = QTableWidget(self)
-                table_widget.layout = QGridLayout(self.tables_tab_widget)
-                self.tables_tab_widget.addTab(table_widget, name)
+                widget = QWidget()
+                self.tables_tab_widget.addTab(widget, name)
+                self.init_table_widget_for_db_table(widget, name)
 
         except sqlite3.Error as e:
             self.statusBar().showMessage(f'Ошибка: {e.args[0]}')
         except Exception as e:
             self.statusBar().showMessage(f'Ошибка: {e.args[0]}')
+
+    def init_table_widget_for_db_table(self, widget, name):
+        layout = QGridLayout()
+
+        table_widget = QTableWidget()
+        # Получим результат запроса,
+        cursor = self.db_connection.cursor()
+        res = cursor.execute(f"SELECT * FROM {name}").fetchall()
+
+        # Если запись не нашлась, то не будем ничего делать
+        if not res:
+            self.statusBar().showMessage('Ничего не нашлось')
+            return
+        # Заполнили размеры таблицы
+        table_widget.setRowCount(len(res))
+        table_widget.setColumnCount(len(res[0]))
+        # Устанавливаем заголовки таблицы
+        titles = [description[0] for description in cursor.description]
+        table_widget.setHorizontalHeaderLabels(titles)
+        # Заполнили таблицу полученными элементами
+        for i, elem in enumerate(res):
+            for j, val in enumerate(elem):
+                table_widget.setItem(i, j, QTableWidgetItem(str(val)))
+
+        table_widget.resizeColumnsToContents()
+
+        layout.addWidget(table_widget)
+
+        widget.setLayout(layout)
 
     def closeEvent(self, event):
         reply = QMessageBox.question(self, 'Закрытие окна', 'Вы уверены, что хотите закрыть приложение?',
